02db171ac76c33e14ea77d9c00d8de539c75d8b5
BincyFranclin <152765498+BincyFranclin@users.noreply.github.com>
2023-12-18 22:21:55 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_string = "Hello, World!"
+substring = my_string[:5]
+print(substring)
+
+
+# In[2]:
+
+
+my_string = "Python is fun"
+substring = my_string[7:9]
+print(substring)
+
+
+# In[4]:
+
+
+my_string = "Programming"
+substring = my_string[3:7]
+print(substring)
+
+
+# In[5]:
+
+
+my_string = "abcdefgh"
+result = my_string[::2]
+print(result)
+
+
+# In[6]:
+
+
+my_string = "abcdefg"
+substring = my_string[-3:]
+print(substring)
+
+
+# In[7]:
+
+
+my_string = "Data Science"
+last_three_characters = my_string[-3:]
+print(last_three_characters)
+
+
+# In[8]:
+
+
+my_string = "abcdefghij"
+result = my_string[::2]
+print(result)
+
+
+# In[9]:
+
+
+my_string = "1234567890"
+result = my_string[1::2]
+print(result)
+
+
+# In[10]:
+
+
+my_string = "Hello"
+reversed_string = my_string[::-1]
+print(reversed_string)
+
+
+# In[11]:
+
+
+my_string = "abcdefghijk"
+substring = my_string[::2]
+print(substring)
+
+
+# In[ ]:
+
+
+
+
